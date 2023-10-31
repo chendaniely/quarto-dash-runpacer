1f53aff1b6cce7fe075ad20b978fb5ef53cd0094
Daniel Chen <chendaniely@users.noreply.github.com>
2023-10-30 23:44:05 -0700

    make it shiny

--- generate.py
+++ generate.py
@@ -1,7 +1,18 @@
 import convert
 
-def generate_reference_range(input_time, range_faster=30, range_slower=30, by=1):
-  seconds = convert.input_time_seconds(input_time, verbose=False)
-  seconds_range = range(int(seconds) - range_faster, int(seconds) + range_slower, by)
+def generate_reference_range(input_time, range_faster=-30, range_slower=30, by=1):
+  """Generate a list of seconds.
 
-  return seconds_range
+  Tries to keep the input_time value in the center of the list when the ranges
+  on both sides are the same.
+  This helps when the `by` is not a multiple of the faster and slower second ranges.
+  """
+  seconds = convert.input_time_seconds(input_time, verbose=False)
+  seconds_range = (
+    list(range(int(seconds), int(seconds) + range_faster, by * -1))
+    + [int(seconds)]
+    + list(range(int(seconds), int(seconds) + range_slower, by))
+  )
+
+   # use set to remove duplicates, then sort
+  return sorted(list(set(seconds_range)))
